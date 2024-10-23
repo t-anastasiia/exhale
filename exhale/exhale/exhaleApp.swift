5943a491a0c072c01b443e9b7c855c93cb3f9510
nas.www.ay <nastaytalmazan@gmail.com>
2024-10-23 20:47:06 +1000

    first commit

---
+++ exhale/exhale/exhaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  exhaleApp.swift
+//  exhale
+//
+//  Created by Анастасия Талмазан on 2024-10-23.
+//
+
+import SwiftUI
+
+@main
+struct exhaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
